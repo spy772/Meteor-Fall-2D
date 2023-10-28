1c803253c1d9fa78d8b99c31ce1c923ad3741bb3
spy772 <edward.tanurkov@gmail.com>
2023-10-27 23:30:34 -0400

    added early running mechanic and falling animation
    - Character can now sprint, with a running animation
    - Currently, the physics is a bit off because when you jump from this state you slow down drastically (to walking speed while jumping)
    - Need to update this at a later time
    - Added a falling animation for when the character stays over the full jumping animation time in the air

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,21 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
     public Animator animator;
-    AnimatorStateInfo animatorState;
+    public PlayerInputs playerInputs;
+    private InputAction move;
+    private InputAction fire;
+    private InputAction sprint;
     
     public float walkSpeed = 20f;
     public float runSpeed = 40f;
+    float groundSpeed;
 
     float horizontalMove = 0f;
     bool isJumping = false;
-
     float speed = 0f;
+    Vector2 moveDirection = Vector2.zero;
+
+
+    private void Awake()
+    {
+        playerInputs = new PlayerInputs();
+    }
+
+    private void OnEnable()
+    {
+        move = playerInputs.Player.Move;
+        sprint = playerInputs.Player.Sprint;
+        move.Enable();
+        sprint.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -28,21 +52,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * walkSpeed;
-        speed = Mathf.Abs(horizontalMove);
+        moveDirection = move.ReadValue<Vector2>();
+        speed = moveDirection.magnitude;
         animator.SetFloat("Speed", speed);
         animator.SetBool("isMoving", speed > 0);
         animator.SetBool("isWalking", speed > 0);
 
-        if (Input.GetButtonDown("Jump")) {
+        if (moveDirection.y > 0) {
             animator.SetBool("isJumping", true);
             isJumping = true;
+        }
+
+        if (sprint.IsPressed() && animator.GetBool("isJumping") == false)
+        {
+            animator.SetBool("isRunning", true);
+            groundSpeed = runSpeed;
+        } else
+        {
+            animator.SetBool("isRunning", false);
+            groundSpeed = walkSpeed;
         }
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.deltaTime, false, isJumping);
+        controller.Move(moveDirection.x * groundSpeed * Time.deltaTime, false, isJumping);
         isJumping = false;
     }
 
